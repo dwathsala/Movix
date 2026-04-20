6a7f8712de1ec0417bfc6868015cee739575607e
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-04-20 13:09:04 +0530

    Add constants for icons and images, define movie interfaces
    - Created `icons.ts` to centralize icon imports.
    - Created `images.ts` to centralize image imports.
    - Added interfaces for `Movie`, `TrendingMovie`, `MovieDetails`, and `TrendingCardProps` in `interfaces.d.ts`.
    - Declared module for PNG imports in `images.d.ts`.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,8 @@ export default function Index() {
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
 
-      <Link href="/onboarding">Onboarding</Link>
+      {/*<Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avenger Movie</Link> */}
     </View>
   );
 }
